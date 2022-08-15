71046772532d449771cd5ac8d66d5d43a095bef3
Prasanth138 <prasanthanbu777@gmail.com>
2022-08-15 17:37:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from 'react';
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import AddUser  from "./components/AddUser";
+import EditUser from "./components/EditUser";
+import { GlobalProvider } from "./context/GlobalState";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
+  const [file, setFile] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" style={{ maxWidth: "30rem", margin: "4rem auto"}}>
+      <GlobalProvider>
+      <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route path="/add" element={<AddUser file={file} setFile={setFile} />} />
+            <Route path="/edit/:id" element={<EditUser file={file} setFile={setFile}/>}/>
+       </Routes>
+      </GlobalProvider>
     </div>
   );
 }
